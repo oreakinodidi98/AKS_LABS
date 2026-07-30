52923fdb78617e7b5db2567ccc044bf5c99d91be
Ore Akin-Odidi <oreakinodidi@microsoft.com>
2026-07-30 14:02:57 +0100

    fix: Update azurerm provider configuration to skip resource provider registration

--- AKS_AI_and_LLM/AI_Runway/infra/provider.tf
+++ AKS_AI_and_LLM/AI_Runway/infra/provider.tf
@@ -48,7 +48,7 @@ terraform {
 
 }
 provider "azurerm" {
-  resource_provider_registrations = "none"
+  skip_provider_registration = true
   features {
     key_vault {
       purge_soft_delete_on_destroy    = false
