52cec7332ccf98ce25ae0c13bd523387308676ed
Ore Akin-Odidi <oreakinodidi@microsoft.com>
2026-07-30 15:39:16 +0100

    fix: use Standard_D4s_v3 for AKS system pool (D4ds variants not allowed in uksouth)

--- AKS_AI_and_LLM/AI_Runway/infra/modules/aks/main.tf
+++ AKS_AI_and_LLM/AI_Runway/infra/modules/aks/main.tf
@@ -69,7 +69,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     max_count            = 6
     temporary_name_for_rotation = "temppool"
       enable_auto_scaling  = true
-    vm_size              = "Standard_D4ds_v5"
+    vm_size              = "Standard_D4s_v3"
     # Node taints for system pool
     # only_critical_addons_enabled = true
     vnet_subnet_id = var.aks_subnet_id 
